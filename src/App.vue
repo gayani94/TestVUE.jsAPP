d3fadbb2dc03e1015b2e6ad7151b521e9f99e2ba
Gayani Nadeesha <gayani.nadee@yahoo.com>
2020-06-03 22:40:06 +0900

    update

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+       <app-header> </app-header>
+
+        <div class="container">
+            <div>{{ name }}</div>
+        </div>
+        
+      <compFooter/>
+    </div>
+</template>
+
+<script>
+    import compFooter from './Components/Header_footer/Footer.vue';
+
+
+    export default {
+        data(){
+            return {
+                name: "Nadeesha"
+            }
+        },
+        components:{
+            compFooter
+        }
+    }
+</script>
+
+<style>
+  body {
+      padding:0;
+      margin:0;
+      font-family: 'Roboto', sans-serif;
+  }
+  .container {
+      min-height: 84vh;
+      box-sizing: border-box;
+      padding:20px;
+  }
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import Header from './Components/Header_footer/Header.vue';
+
+
+Vue.component('app-header',Header)
+
+
+new Vue({
+   el:'#app',
+   render: h => h(App)
+})
